024c1343987c98aa482223cd85db6133aa4fdd26
Cellsito <celsobfigueroa@gmail.com>
2024-11-04 17:18:07 -0300

    Feat: exercício 6

---
+++ Assets/Scripts/exercicio06.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class exercicio06 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] bool Guerreiro;
+    [SerializeField] bool Mago;
+    void Start()
+    {
+        if (Guerreiro == true && Mago == false) 
+        {
+            print("Guerreiro escolhido");
+        }
+
+        else if (Guerreiro == false && Mago == true)
+        {
+            print("Mago escolhido");
+        }
+
+        else if (Guerreiro == true && Mago == true)
+        {
+            print("Não pode escolher os Dois");
+        }
+        else
+        {
+            print("Escolha algum personagem");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
