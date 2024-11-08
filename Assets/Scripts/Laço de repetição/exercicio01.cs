64b9ba01f73cc966caf740d9fc9a8bb31607b9d3
Cellsito <celsobfigueroa@gmail.com>
2024-11-08 16:33:28 -0300

    Feat: Exercício 1

---
+++ Assets/Scripts/Laço de repetição/exercicio01.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class exercicio01 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int num = 5;
+
+        while (num >= 1)
+        {
+            print(num);
+            num--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
